5d23a3ab860e77b1f5a44d0a62b3172609f57145
xiaolu1990 <luzhangshun@web.de>
2017-10-22 22:32:06 +0200

    lesson2
    pyqt application structure

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-app = QApplication(sys.path)
-window = QWidget()
-window.setGeometry(100, 100, 400, 300)
-window.setWindowTitle("PyQt5")
-window.show()
+class Window(QMainWindow):
+	"""a normal window"""
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setGeometry(100, 100, 400, 400)
+		self.setWindowTitle("PyQt5")
+		self.show()
+
+app = QApplication(sys.argv)
+GUI = Window()
 
 sys.exit(app.exec_())
