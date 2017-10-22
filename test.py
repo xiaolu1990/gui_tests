4842628840751ecaaacbae4f286c8e3b428b00ee
xiaolu1990 <luzhangshun@web.de>
2017-10-22 22:51:14 +0200

    lesson 4
    Button Functions

--- test.py
+++ test.py
@@ -13,12 +13,18 @@ class Window(QMainWindow):
 
 	def home(self):
 		btn = QPushButton("Quit", self)
-		btn.resize(100, 100)
-		btn.move(100, 100)
-		btn.clicked.connect(QCoreApplication.instance().quit)
+		btn.clicked.connect(self.close_application)
+		btn.resize(btn.sizeHint())  #automatically set the button size
+		btn.move(0, 0)		
 		self.show()
 
-app = QApplication(sys.argv)
-GUI = Window()
+	def close_application(self):
+		print("Bye Bye")
+		sys.exit()
 
-sys.exit(app.exec_())
+def run():
+	app = QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
+
+run()
