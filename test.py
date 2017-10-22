83002b9c727ac309c4d3e4d7a0d050d986750f60
xiaolu1990 <luzhangshun@web.de>
2017-10-22 22:42:52 +0200

    lesson3
    pyqt buttons

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import *
 
 class Window(QMainWindow):
 	"""a normal window"""
@@ -7,6 +9,13 @@ class Window(QMainWindow):
 		super(Window, self).__init__()
 		self.setGeometry(100, 100, 400, 400)
 		self.setWindowTitle("PyQt5")
+		self.home()
+
+	def home(self):
+		btn = QPushButton("Quit", self)
+		btn.resize(100, 100)
+		btn.move(100, 100)
+		btn.clicked.connect(QCoreApplication.instance().quit)
 		self.show()
 
 app = QApplication(sys.argv)
