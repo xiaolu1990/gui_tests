e31d80d28c44e9b243d006c1547f5a6fa6ad7b7a
xiaolu1990 <luzhangshun@web.de>
2017-10-23 10:11:29 +0200

    updated lesson 5

--- test.py
+++ test.py
@@ -4,40 +4,45 @@ from PyQt5.QtWidgets import QAction
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import *
 
+
 class Window(QMainWindow):
-	"""a normal window"""
-	def __init__(self):
-		super(Window, self).__init__()
-		self.setGeometry(100, 100, 400, 400)
-		self.setWindowTitle("PyQt5")
+    """a normal window"""
 
-		extractAction = QAction("Exit", self)
-		extractAction.setShortcut("Ctrl+Q")
-		extractAction.setStatusTip("Leave the App")
-		extractAction.triggered.connect(self.close_application)
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(100, 100, 400, 400)
+        self.setWindowTitle("PyQt5")
 
-		self.statusBar()
+        extractAction = QAction("Exit", self)
+        extractAction.setShortcut("Esc")
+        extractAction.setStatusTip("Leave the App")
+        extractAction.triggered.connect(self.close_application)
 
-		mainMenu = self.menuBar()
-		fileMenu = mainMenu.addMenu("File")
-		fileMenu.addAction(extractAction)
+        self.statusBar()
 
-		self.home()
+        mainMenu = self.menuBar()
+        mainMenu.setNativeMenuBar(False)	#show the menubar on MacOS looks like on Windows
+        fileMenu = mainMenu.addMenu("File")
+        fileMenu.addAction(extractAction)
 
-	def home(self):
-		btn = QPushButton("Quit", self)
-		btn.clicked.connect(self.close_application)
-		btn.resize(btn.sizeHint())  #automatically set the button size
-		btn.move(100, 100)		
-		self.show()
+        self.home()
 
-	def close_application(self):
-		print("Bye Bye")
-		sys.exit()
+    def home(self):
+        btn = QPushButton("Quit", self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(btn.sizeHint())  # automatically set the button size
+        btn.move(100, 100)
+        self.show()
+
+    def close_application(self):
+        print("Bye Bye")
+        sys.exit()
+
 
 def run():
-	app = QApplication(sys.argv)
-	GUI = Window()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    GUI = Window()
+    sys.exit(app.exec_())
+
 
 run()
